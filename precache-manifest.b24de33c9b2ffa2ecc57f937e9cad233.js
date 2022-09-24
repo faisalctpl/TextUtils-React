4962c2ce8506fe36ec4228d9349875e0f7fe533a
faisal <Faisal@faisalkhan190162.com>
2022-09-24 10:45:02 +0500

    Updates

---
+++ precache-manifest.b24de33c9b2ffa2ecc57f937e9cad233.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e053a40ad570e27ed244",
+    "url": "/TextUtils-React/static/css/main.324d2f92.chunk.css"
+  },
+  {
+    "revision": "e053a40ad570e27ed244",
+    "url": "/TextUtils-React/static/js/main.e053a40a.chunk.js"
+  },
+  {
+    "revision": "c972895fac6c9db70f84",
+    "url": "/TextUtils-React/static/js/1.c972895f.chunk.js"
+  },
+  {
+    "revision": "56c1b9c6cb5c47266959",
+    "url": "/TextUtils-React/static/js/2.56c1b9c6.chunk.js"
+  },
+  {
+    "revision": "96db25503b37c21d4ae6",
+    "url": "/TextUtils-React/static/js/runtime~main.96db2550.js"
+  },
+  {
+    "revision": "feb549acf647ef19bb45b338f3755c8a",
+    "url": "/TextUtils-React/index.html"
+  }
+];
